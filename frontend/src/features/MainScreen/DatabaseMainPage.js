64662f51b3cf868c38c13cc50e9787bb3617d757
emival12 <vale-12.p@hotmail.it>
2025-11-07 12:35:21 +0100

    Fix a bug on landing page with old values

--- frontend/src/features/MainScreen/DatabaseMainPage.js
+++ frontend/src/features/MainScreen/DatabaseMainPage.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Outlet, useParams } from "react-router-dom";
 import TablesSidebar from "../TablesSidebar/TablesSidebar";
