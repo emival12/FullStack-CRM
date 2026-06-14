87f42c85d4fd8d007c5472d8c232625482e59a56
emival12 <vale-12.p@hotmail.it>
2026-06-14 10:10:11 +0200

    Remove dead code

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-//import reportWebVitals from "./reportWebVitals";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
@@ -32,5 +31,3 @@ root.render(
     </ErrorBoundary>
   </React.StrictMode>,
 );
-
-//reportWebVitals(console.log);
